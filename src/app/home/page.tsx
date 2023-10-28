cd0f467b3829dfd23dbff2ca206935878a68ea7e
tjuandev <tpydeveloper@gmail.com>
2023-10-27 23:56:25 -0300

    style: first content of home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,15 @@
+import { Logo } from 'components/atoms'
+import S from './styles.module.scss'
+
 const Home = () => {
-  return <h1>Test</h1>
+  return (
+    <div className={S.container}>
+      <Logo size="lg" />
+      <h4 className={S.info_text}>
+        Find <strong>anything</strong> you want about this awesome universe!
+      </h4>
+    </div>
+  )
 }
 
 export default Home
--- src/theme/fontFamily.ts
+++ src/theme/fontFamily.ts
@@ -3,7 +3,7 @@ import { Poppins } from 'next/font/google'
 const poppins = Poppins({
   display: 'swap',
   subsets: ['latin', 'latin-ext'],
-  weight: ['400', '500', '600']
+  weight: ['400', '500', '600', '700']
 })
 
 export default poppins
