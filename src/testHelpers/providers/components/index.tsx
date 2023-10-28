6f2c9a83e161e4cdef39db86a360450bd2132b53
tjuandev <tpydeveloper@gmail.com>
2023-10-27 22:33:16 -0300

    test: fixed tests not working

--- src/testHelpers/providers/components/index.tsx
+++ src/testHelpers/providers/components/index.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from 'react'
 import { render, type RenderResult } from '@testing-library/react'
-import MainProvider from 'providers/mainProvider'
+import MainProvider from 'providers/MainProvider'
 
 const renderWithProviders = (children: ReactNode): RenderResult =>
   render(<MainProvider>{children}</MainProvider>)
