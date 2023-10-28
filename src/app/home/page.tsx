94d95d87fd4106cae402bbcd686e626d123b5199
tjuandev <tpydeveloper@gmail.com>
2023-10-28 18:17:11 -0300

    refactor: improved home page code

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,24 +3,32 @@ import S from './styles.module.scss'
 import { Card } from 'components/molecules'
 import { categoriesList } from 'constant'
 
+const Categories = () => {
+  return (
+    <div className={S.categories}>
+      {categoriesList.map(({ title, src }) => (
+        <Card
+          key={title}
+          src={src}
+          title={title}
+          alt={`${title}'s image`}
+          href={`/categories/${title}`}
+        />
+      ))}
+    </div>
+  )
+}
+
 const Home = () => {
   return (
     <div className={S.container}>
-      <Logo size="lg" />
+      <header>
+        <Logo size="lg" />
+      </header>
       <h4 className={S.info_text}>
         Find <strong>anything</strong> you want about this awesome universe!
       </h4>
-      <div className={S.categories}>
-        {categoriesList.map(({ title, src }) => (
-          <Card
-            key={title}
-            src={src}
-            title={title}
-            alt={`${title}'s image`}
-            href={`/categories/${title}`}
-          />
-        ))}
-      </div>
+      <Categories />
     </div>
   )
 }
