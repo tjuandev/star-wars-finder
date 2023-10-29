1740b500a08f707e85cb840f255bdde4bae5ed01
tjuandev <tpydeveloper@gmail.com>
2023-10-29 12:55:59 -0300

    test: removed unnecessary tests

--- src/components/atoms/Button/__tests__/Button.test.tsx
+++ src/components/atoms/Button/__tests__/Button.test.tsx
@@ -1,4 +1,3 @@
-import S from '../styles.module.scss'
 import { screen } from '@testing-library/react'
 import renderWithProviders from 'testHelpers/providers/components'
 import { Button } from '..'
@@ -8,23 +7,5 @@ describe('[Component:Atoms] Button', () => {
     renderWithProviders(<Button icon={<div>icon</div>}>Button</Button>)
     const icon = screen.getByText('icon')
     expect(icon).toBeInTheDocument()
-  })
-
-  it('should attach button to left', () => {
-    renderWithProviders(
-      <Button icon={<div>icon</div>} attachPos="left">
-        Button
-      </Button>
-    )
-
-    const button = screen.getByText('Button')
-    expect(button).toHaveClass(S.attach_left)
-  })
-
-  it('should be uppercase', () => {
-    renderWithProviders(<Button uppercase>Button</Button>)
-
-    const button = screen.getByText('Button')
-    expect(button).toHaveClass(S.uppercase)
   })
 })
