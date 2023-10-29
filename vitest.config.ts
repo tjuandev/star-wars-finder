c5c4a9ef5cda533da301a2a06792b1bdfee056b9
tjuandev <tpydeveloper@gmail.com>
2023-10-29 14:50:27 -0300

    test: excluded redux storage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-    plugins: [tsconfigPaths(), react()],
-    test: {
-      environment: 'happy-dom',
-      globals: true,
-      passWithNoTests: true,
-      setupFiles: ['./vitest.setup.ts']
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    environment: 'happy-dom',
+    globals: true,
+    passWithNoTests: true,
+    setupFiles: ['./vitest.setup.ts'],
+    coverage: {
+      exclude: ['src/store/storage.ts']
     }
+  }
 })
