2491d2903bd80d0e98c113241a6f3c3ff147e1fa
tjuandev <tpydeveloper@gmail.com>
2023-10-30 23:15:16 -0300

    style: improved responsiveness

--- src/components/atoms/Logo/index.tsx
+++ src/components/atoms/Logo/index.tsx
@@ -3,5 +3,5 @@ import type { LogoProps } from './types'
 import S from './styles.module.scss'
 
 export const Logo = ({ size = 'md' }: LogoProps) => (
-  <LogoIcon aria-label="logo" role="img" className={S[size]} />
+  <LogoIcon aria-label="logo" role="img" className={S[`logo_${size}`]} />
 )
